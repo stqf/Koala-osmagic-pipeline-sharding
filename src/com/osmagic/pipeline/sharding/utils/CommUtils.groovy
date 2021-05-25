74f80a990d6a7fc7788a4f9cc273594329894c23
yanying <yanying@yourangroup.com>
2021-05-25 20:46:43 +0800

    feat:添加一个用于查询需处理模块的方法

--- src/com/osmagic/pipeline/sharding/utils/CommUtils.groovy
+++ src/com/osmagic/pipeline/sharding/utils/CommUtils.groovy
@@ -26,4 +26,38 @@ class CommUtils {
         return requireItem;
     }
 
+    /**
+     * 需要处理的项目
+     * @param items
+     * @param params
+     * @return
+     */
+    static List<Map> requireHandlerProjects(List<Map> items, Map params) {
+        List<Map> reItems = new ArrayList<>()
+        for (Map item : items) {
+            String nameItem = item.get("project")
+            if (isRequireHandler(nameItem, params)) {
+                reItems.add(item)
+            }
+        }
+
+        for (Map reItem : reItems) {
+            String nameItem = reItem.get("project")
+            List<Map> deployItems = reItem.get("deploy")
+            List<Map> requireItems = new ArrayList<>()
+            for (Map item : deployItems) {
+
+                def suffixItem = item.get("name")
+                def kItem = suffixItem == null ? nameItem : "$nameItem-$suffixItem"
+                def requireIt = isRequireHandler(kItem, params)
+                if (requireIt) {
+                    requireItems.add(item)
+                }
+            }
+            reItem.put("deploy", requireItems)
+        }
+
+        return reItems
+    }
+
 }
