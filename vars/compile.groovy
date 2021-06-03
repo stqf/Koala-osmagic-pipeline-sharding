6bdf0ba24647ebbdf589b721dcaf0b96c791b1c2
yanying <yanying@yourangroup.com>
2021-06-03 14:54:39 +0800

    feat:优化编译过程中的日志

--- vars/compile.groovy
+++ vars/compile.groovy
@@ -1,36 +1,34 @@
 import com.osmagic.pipeline.sharding.utils.CommUtils
 
 def compile(String project) {
+    def ts = System.currentTimeMillis()
     String path = "${env.WORKSPACE}/$project"
     def file = new File(path)
     def items = file.list()
-    println("Build[$project] start ... ")
+    println("[TRACE] handler start : name is $project ")
     if (items.contains("pom.xml")) {
         /*Maven项目编译*/
-        println("[$project] Maven ")
-        sh """
-            cd $project && pwd && mvn package -T 3C 
-        """
+        def shItem = "cd $project && pwd && mvn package -T 3C"
+        println("[DEBUG] compile Java: use type is Maven, name is $project, command is '$shItem'")
+        sh "$shItem"
     } else if (items.contains("build.gradle")) {
         /*Gradle项目编译*/
-        println("[$project] Gradle ")
         def reItem = sh(script: "which gradle", returnStatus: true)
         def bashItem = reItem == 0 ? "gradle" : "bash /var/jenkins_home/gradle-6.8.3/bin/gradle"
-        sh """
-            cd $project && pwd && $bashItem build -x test --parallel
-        """
+        def shItem = "cd $project && pwd && $bashItem build -x test --parallel"
+        println("[DEBUG] compile Java: use type is Gradle, name is $project, command is '$shItem'")
+        sh "$shItem"
     } else if (items.contains("package.json")) {
         def wctItem = Optional.ofNullable(env.WebCompileType).orElse("npm")
+        def shItem = "cd $project && $wctItem install && $wctItem run build"
         /*Web项目编译*/
-        println("[DEBUG] compile web, use type is $wctItem, name is $project")
-        sh """
-            cd $project && pwd && npm install && npm run build
-        """
+        println("[DEBUG] compile Web : use type is $wctItem, name is $project, command is '$shItem'")
+        sh "$shItem"
     } else {
-        println("[$project] Unknown ")
+        println("[ERROR] compile Unknown: use type is Unknown, name is $project")
         error "不能识别的项目[$project], 请联系管理员 ... "
     }
-    println("Compile[$project] finish ... ")
+    println("[DEBUG] handler finish: name is $project, time is ${System.currentTimeMillis() - ts}ms")
 }
 
 def compiles(List projects) {
@@ -38,9 +36,8 @@ def compiles(List projects) {
     projects.each {
 
         def nameItem = it.get("project")
-
         def requireItem = CommUtils.isRequireHandler(nameItem, params)
-        println("Compile $nameItem ... requireItem: $requireItem ")
+        println("[TRACE] Compile project start : name is $nameItem, require is $requireItem ")
 
         /*判断项目是否需要编译*/
         if (!requireItem) {
@@ -49,7 +46,7 @@ def compiles(List projects) {
 
         tasks."Project[$nameItem]" = {
             compile(nameItem)
-            echo "Project[$nameItem] Compile finish ..."
+            println("[INFO ] Compile project finish: name is $nameItem ")
         }
     }
 
